b0935dedc27730281109c266122bf480f8d00722
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-18 15:30:03 +0200

    Correct errors

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-class app
+class App
+
 include BookLabelUtilities 
 
 def show_options
@@ -21,6 +22,17 @@ def add_items(number)
     case number
     when 7
         create_book
+    end
+end
+
+def pick_action(number)
+    if number <= 6
+        list_items(number)
+    else
+        add_items(number)
+    end
+end
+
 
 def run
     puts "Welcome to our catalog of things"
@@ -28,5 +40,14 @@ def run
     until exit
         show_options
         number = gets.to_i
+        if number < 10
+            pick_action(number)
+        elsif number == 10
+            exit = true
+            puts "Thank you , see you soon bye!"
+        else
+            puts "Error friend, wrong input. Sorry I don't make the rules ¯\\(ツ)/¯"
+        end
+    end
 end
-
+end
