ee4199dda963909d816dbf6b426871db4944b195
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-18 22:17:25 +0200

    Correct linters error

--- class/book.rb
+++ class/book.rb
@@ -1,29 +1,27 @@
 class Publication
-  
-    attr_reader :publish_date, :id
-  
-    def initialize(publish_date, id, archived)
-      @publish_date = publish_date
-      @id = id
-      @archived = archived
-    end
-  
-    def can_be_archived?
-      @archived
-    end
+  attr_reader :publish_date, :id
+
+  def initialize(publish_date, id, archived)
+    @publish_date = publish_date
+    @id = id
+    @archived = archived
   end
-  
-  class Book < Publication
-    attr_reader :publisher, :cover_state
-  
-    def initialize(publish_date, publisher, cover_state, id = Random.rand(100_000))
-      super(publish_date, id, false) # Call the parent class's initialize method
-      @publisher = publisher
-      @cover_state = cover_state
-    end
-  
-    def can_be_archived?
-      super || cover_state == 'bad'
-    end
+
+  def can_be_archived?
+    @archived
   end
-  
+end
+
+class Book < Publication
+  attr_reader :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state, id = Random.rand(100_000))
+    super(publish_date, id, false) # Call the parent class's initialize method
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+end
--- class/label.rb
+++ class/label.rb
@@ -1,28 +1,27 @@
 class Label
-  
-    attr_reader :id, :title, :color, :items
-  
-    def initialize(title, color, items = [], id = Random.rand(100_000))
-      @id = id
-      @title = title
-      @color = color
-      @items = items
-    end
-  
-    def add_item(item)
-      @items << item
-      item.label = self # Set the label attribute on the item
-    end
+  attr_reader :id, :title, :color, :items
+
+  def initialize(title, color, items = [], id = Random.rand(100_000))
+    @id = id
+    @title = title
+    @color = color
+    @items = items
   end
-  
-  class Book
-    attr_accessor :id, :publish_date, :publisher, :cover_state, :label
-  
-    def initialize(publish_date, publisher, cover_state)
-      @id = Random.rand(100_000)
-      @publish_date = publish_date
-      @publisher = publisher
-      @cover_state = cover_state
-      @label = nil
-    end
+
+  def add_item(item)
+    @items << item
+    item.label = self # Set the label attribute on the item
   end
+end
+
+class Book
+  attr_accessor :id, :publish_date, :publisher, :cover_state, :label
+
+  def initialize(publish_date, publisher, cover_state)
+    @id = Random.rand(100_000)
+    @publish_date = publish_date
+    @publisher = publisher
+    @cover_state = cover_state
+    @label = nil
+  end
+end
