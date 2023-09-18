e11e8237ea7699570d14f41e95b15bb6d827bf09
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-18 23:10:55 +0200

    Fix bug

--- class/book.rb
+++ class/book.rb
@@ -16,7 +16,7 @@ class Book < Publication
   attr_reader :publisher, :cover_state
 
   def initialize(publish_date, publisher, cover_state, id = Random.rand(100_000))
-    super(publish_date, id, false) 
+    super(publish_date, id, false)
     @publisher = publisher
     @cover_state = cover_state
   end
