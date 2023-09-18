a6aba3a539da98ecfbb1740933b8a8628c54818a
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-18 23:21:16 +0200

    Create book storage

--- class/book.rb
+++ class/book.rb
@@ -24,4 +24,17 @@ class Book < Publication
   def can_be_archived?
     super || cover_state == 'bad'
   end
+
+  def to_json(*args)
+    {
+        "id" => @id,
+        "publish_date" => @publish_date,
+        "publisher" => @publisher,
+        "cover_state" => @cover_state,
+        "label" => @label
+    }.to_json(*args)
+    
+  end
 end
+
+
