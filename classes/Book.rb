622ee8036db3ed7f2fea29286000c1a205f2ea9b
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-18 13:58:04 +0200

    Add publish date

--- classes/Book.rb
+++ classes/Book.rb
@@ -1,13 +1,13 @@
 class Book
     attr_reader :publisher, :cover_state
-     
-    def initialize(publisher, cover_state, id = Random.rand(10_0000))
-        super(id, false)
+
+    def initialize(publish_date, publisher, cover_state, id = Random.rand(10_0000))
+        super(publish_date, id, false)
         @publisher = publisher
         @cover_state = cover_state
     end
 
-    def can_be_archive?
+    def can_be_archived?
         super || cover_state == "bad"
     end
 end
