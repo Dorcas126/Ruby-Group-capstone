e779cb2a954baaf2108e5203a07bf284d1227474
DevaWinner <winneraaniekan@gmail.com>
2023-09-20 14:10:43 +0100

    Define add music album in main module

--- Modules/main_module.rb
+++ Modules/main_module.rb
@@ -10,4 +10,20 @@ module MainModule
     app.create_game(multiplayer, last_played_at, publish_date)
   end
 
+  def add_mucicalbum(app)
+    puts "\n"
+    puts 'Is it on spotify [True/False]:'
+    on_spotify = gets.chomp
+    if %w[True true].include?(on_spotify)
+      on_spotify = true
+    elsif %w[False false].include?(on_spotify)
+      on_spotify = false
+    else
+      puts 'Invalid input'
+    end
+    puts 'When is album published [Enter date format year/month/date]:'
+    publish_date = gets.chomp
+    app.create_musicalbum(publish_date, on_spotify)
+  end
+
 end
