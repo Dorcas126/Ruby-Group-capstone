ada62210b63de33159d42980da3f0a757521f28e
DevaWinner <winneraaniekan@gmail.com>
2023-09-20 14:09:39 +0100

    Define action options

--- main.rb
+++ main.rb
@@ -18,3 +18,29 @@ class Main
     puts '9. Add a music album'
     puts '10. End'
   end
+
+  def main
+    app = App.new
+    loop do
+      list_options
+      option = gets.chomp.to_i
+      options = {
+        1 => -> { app.list_books }, 2 => -> { app.list_games }, 3 => -> { app.list_musicalbums },
+        4 => -> { app.list_genres },
+        5 => -> { app.list_authors },
+        6 => -> { app.list_labels },
+        7 => -> { add_book(app) },
+        8 => -> { add_game(app) },
+        9 => -> { add_mucicalbum(app) },
+        10 => -> { exit }
+      }
+      case option
+      when 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        options[option].call
+      end
+    end
+  end
+end
+
+main = Main.new
+main.main
