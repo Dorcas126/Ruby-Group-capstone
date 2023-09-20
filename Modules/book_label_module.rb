fa473554696241edc6aacb49b42d807953fa8687
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-20 18:42:49 +0200

    List all books,list all labels ,add a book.

---
+++ Modules/book_label_module.rb
@@ -0,0 +1,67 @@
+require_relative '../classes/book'
+require_relative '../classes/label'
+
+module BookLabelUtilities
+  def initialize
+    @books = []
+    @labels = []
+  end
+
+  def create_label
+    puts 'Enter the label title: '
+    title = gets.chomp
+    puts 'Enter the color: '
+    color = gets.chomp
+
+    label = Label.new(title, color)
+    @labels.push(label)
+    label
+  end
+
+  def create_book
+    puts 'When was the book published?(year/month/date): '
+    publish_date = gets.chomp
+    puts 'Enter publisher name: '
+    publisher = gets.chomp
+    puts 'What is the book cover state (good or bad): '
+    cover_state = gets.chomp
+    puts 'Enter the label title: '
+    title = gets.chomp
+    puts 'Enter the color: '
+    color = gets.chomp
+
+
+    book = Book.new(publish_date, publisher, cover_state, title, color)
+    label = create_label
+    label.add_item(book)
+    @books.push(book)
+
+    puts 'Book has been created successfully'
+end
+
+  def list_books
+    if @books.empty?
+      puts 'Book list is empty'
+    else
+      @books.each_with_index do |book, index|
+        puts "#{index}
+                  ID: #{book.id}
+                  publish_date: #{book.publish_date}
+                  cover_state: #{book.cover_state}"
+      end
+    end
+  end
+
+  def list_labels
+    if @labels.empty?
+      puts 'Label list is empty'
+    else
+      @labels.each_with_index do |label, index|
+        puts "#{index}
+                  ID: #{label.id},
+                  title: #{label.title},
+                  color: #{label.color}\n"
+      end
+    end
+  end
+end
