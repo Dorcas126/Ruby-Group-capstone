9fb72a55c6e467cae40f3475be5eb9c4dac82e1e
DevaWinner <winneraaniekan@gmail.com>
2023-09-20 13:22:40 +0100

    Create list music albums in music module

--- Modules/music_module.rb
+++ Modules/music_module.rb
@@ -7,5 +7,14 @@ module MusicModule
     save_music_album_to_file(@songs)
   end
 
+  def list_musicalbums
+    if @songs.empty?
+      puts 'There are no music album'
+    else
+      @songs.each_with_index do |song, i|
+        puts "#{i + 1}) Is it on spotify?: #{song.on_spotify}, Publish Date: #{song.publish_date}"
+      end
+    end
+  end
 
 end
