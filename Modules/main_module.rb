c9be40c18b7d9cc44e8e4d04f8146ad206b4100d
DevaWinner <winneraaniekan@gmail.com>
2023-09-20 14:10:57 +0100

    Define add book in main module

--- Modules/main_module.rb
+++ Modules/main_module.rb
@@ -26,4 +26,14 @@ module MainModule
     app.create_musicalbum(publish_date, on_spotify)
   end
 
+  def add_book(app)
+    puts "\n"
+    puts 'Who is the publisher [Enter Publisher Name]:'
+    publisher = gets.chomp
+    puts 'What Is the cover\'s state like? [Enter good or bad]:'
+    cover_state = gets.chomp
+    puts 'When was the book published [Enter date format year/month/date]:'
+    publish_date = gets.chomp
+    app.create_book(publisher, cover_state, publish_date)
+  end
 end
