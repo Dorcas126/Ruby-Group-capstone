2cdaca7734eea52b516f6fd1a9737ecafe92adc0
DevaWinner <winneraaniekan@gmail.com>
2023-09-20 14:10:19 +0100

    Define add game method in main module

--- Modules/main_module.rb
+++ Modules/main_module.rb
@@ -1,3 +1,13 @@
 module MainModule
-  
+  def add_game(app)
+    puts "\n"
+    puts 'Is game multiplayes [Yes/No]:'
+    multiplayer = gets.chomp
+    puts 'When is game last played [Enter date format year/month/date]:'
+    last_played_at = gets.chomp
+    puts 'When is game published [Enter date format year/month/date]:'
+    publish_date = gets.chomp
+    app.create_game(multiplayer, last_played_at, publish_date)
+  end
+
 end
