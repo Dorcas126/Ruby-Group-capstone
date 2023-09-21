6e6d5ede5201c2043e9014ecb66dff3b5775252d
DevaWinner <winneraaniekan@gmail.com>
2023-09-21 15:17:28 +0100

    Add genre and game in item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,28 +1,38 @@
 require 'date'
 
-# Item class
 class Item
-  attr_accessor :publish_date, :id
-  attr_reader :archived, :label
+  attr_accessor :id, :publish_date
+  attr_reader :archived, :genre, :author, :label
 
-  def initialize(publish_date, archived, _id = SecureRandom.uuid)
-    @id = SecureRandom.uuid
+  def initialize(publish_date, archived: false, id: Random.rand(1...1000))
+    @id = id
     @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
   def label=(label)
     @label = label
-    label.add_item(self) unless label.items.include?(self)
+    label.items << self unless label.items.include?(self)
   end
-
-  def can_be_archived?
-    Date.today.year - @publish_date.year > 10
-  end
-
-  private :can_be_archived?
 
   def move_to_archive
     @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?()
+    difference = Time.now.year - @publish_date.year.to_i
+    difference > 10
   end
 end
